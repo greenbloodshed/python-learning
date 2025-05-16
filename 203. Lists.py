038d42ac3c57d98669fbf1689e009c0e894f50bf
Green <green_bloodshed@protonmail.com>
2025-05-16 04:23:41 -0400

    Create 203. Lists.py

---
+++ 203. Lists.py
@@ -0,0 +1,20 @@
+#==========================================================================================
+# Lists
+#==========================================================================================
+
+# Creating a list
+fruits = ["apple", "banana", "cherry"]
+
+# Python uses zero-based indexing. Above, apple is 0, banana is 1, cherry is 2
+# Accessing list items
+print(fruits[0]) # Output: apple
+
+# Adding an item to the end of the list
+fruits.append("orange")
+
+# Removing an item from the list
+fruits.remove("banana")
+
+# Iteration
+for fruit in fruits:
+    print(fruit)
