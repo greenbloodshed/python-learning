37a88b3192f0427e0ac130c9b8e029cabd48640c
Green <green_bloodshed@protonmail.com>
2025-05-16 17:51:12 -0400

    Update 102. Conditional Statement: Odd or Even?.py

--- 102. Conditional Statement: Odd or Even?.py
+++ 102. Conditional Statement: Odd or Even?.py
@@ -1,6 +1,9 @@
-'''This code checks whether or not a number is even. It gets a number from user input and stores it as the variable 'number', and converts the string to an integer. 
-Then, the code checks if 'number' divided by 2 has a remainder of 0 using the modulo operator(%) and if so, it outputs: "This is an even number." Otherwise, it prints: "This is an odd number."
-'''
+#==============================================================================================================
+# Conditional statement
+#==============================================================================================================
+
+''' This code checks whether or not a number is even. It gets a number from user input and stores it as the variable 'number', and converts the string to an integer.
+    Then, the code checks if 'number' divided by 2 has a remainder of 0 using the modulo operator(%) and if so, it outputs: "This is an even number." Otherwise, it prints: "This is an odd number."'''
 
 # Get the number from the user and convert it to an integer
 number = int(input("Enter a number: "))
